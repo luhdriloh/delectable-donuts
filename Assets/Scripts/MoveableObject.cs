4bceb857ce4bab51932d3b8eca9326e1bdd8c7a6
luhdriloh <007.sam.moore@gmail.com>
2019-01-19 21:36:12 -0700

    first 5 levels of the game

---
+++ Assets/Scripts/MoveableObject.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction
+{ 
+    UP = 0,
+    RIGHT,
+    DOWN,
+    LEFT
+}
+
+public class MoveableObject : MonoBehaviour
+{
+    private Vector2 _newPosition;
+    private Rigidbody2D _rigidbody;
+
+    private readonly Dictionary<Direction, Vector3> _directionToVectorMap = new Dictionary<Direction, Vector3>
+    {
+        { Direction.UP, Vector3.up },
+        { Direction.LEFT, Vector3.left },
+        { Direction.DOWN, Vector3.down },
+        { Direction.RIGHT, Vector3.right }
+    };
+
+    private void Start ()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _newPosition = transform.position;
+	}
+
+    private void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_newPosition);
+    }
+
+    public bool MoveableInDirection(Direction direction)
+    {
+        Vector3 raycast = transform.position + (Vector3)_directionToVectorMap[direction];
+        RaycastHit2D[] hits = new RaycastHit2D[1];
+        // create a filter for the raycast
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.SetLayerMask(LayerMask.GetMask("Bounds"));
+        int collidersHit = Physics2D.Raycast(raycast, raycast, filter, hits, .1f);
+        if (collidersHit > 0)
+        {
+            return false;
+        }
+
+        // the colliders hit will be stored in hits
+        filter.SetLayerMask(LayerMask.GetMask("Moveable"));
+        collidersHit = Physics2D.Raycast(raycast, raycast, filter, hits, .1f);
+
+
+        if (collidersHit > 0)
+        {
+            MoveableObject moveableObject = hits[0].collider.gameObject.GetComponent<MoveableObject>();
+            if (moveableObject.ObjectsInDirection(direction) == false)
+            {
+                moveableObject.MoveInDirection(direction);
+                return true;
+            }
+        }
+
+        return collidersHit == 0;
+    }
+
+    public bool ObjectsInDirection(Direction direction)
+    {
+        Vector3 raycast = transform.position + (Vector3)_directionToVectorMap[direction];
+        RaycastHit2D[] hits = new RaycastHit2D[1];
+
+        // create a filter for the raycast
+        ContactFilter2D filter = new ContactFilter2D();
+        filter.SetLayerMask(LayerMask.GetMask("Moveable", "Bounds", "Donut"));
+
+        // the colliders hit will be stored in hits
+        int collidersHit = Physics2D.Raycast(raycast, raycast, filter, hits, .1f);
+
+        return collidersHit > 0;
+    }
+
+    public void MoveInDirection(Direction direction)
+    {
+        _newPosition = (Vector3)_newPosition + _directionToVectorMap[direction];
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    public int _nextLevel;
+
+    private MoveableObject _moveableObject;
+    private Rigidbody2D _rigidbody;
+    private Vector2 _newPosition;
+
+    private bool _flip;
+    private bool _rightFacing;
+
+	private void Start ()
+    {
+        _moveableObject = GetComponent<MoveableObject>();
+        _newPosition = transform.position;
+        _rigidbody = GetComponent<Rigidbody2D>();
+
+        _flip = false;
+        _rightFacing = true;
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && _moveableObject.MoveableInDirection(Direction.LEFT) == true)
+        {
+            if (_rightFacing)
+            {
+                _rightFacing = false;
+                _flip = true;
+            }
+
+            _newPosition += Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && _moveableObject.MoveableInDirection(Direction.UP) == true)
+        {
+            _newPosition += Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) && _moveableObject.MoveableInDirection(Direction.RIGHT) == true)
+        {
+            if (_rightFacing == false)
+            {
+                _rightFacing = true;
+                _flip = true;
+            }
+            _newPosition += Vector2.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) && _moveableObject.MoveableInDirection(Direction.DOWN) == true)
+        {
+            _newPosition += Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.R))
+        {
+            Restart();
+        }
+
+        if (_flip)
+        {
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, 0f);
+            _flip = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        _rigidbody.MovePosition(_newPosition);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Donut"))
+        {
+            Debug.Log("level won");
+            collision.gameObject.SetActive(false);
+            SceneManager.LoadScene(_nextLevel.ToString());
+        }
+    }
+
+    private void Restart()
+    { 
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
